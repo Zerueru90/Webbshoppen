65ce11dc4774016d635bb5a5e248d27e304fe8d9
Samson <31880780+Zerueru90@users.noreply.github.com>
2021-09-23 13:31:07 +0200

    pågående

--- index.js
+++ index.js
@@ -45,31 +45,102 @@ Vue.component('shopitems', {
             }
                 
         },
-            methods: {
-            shopItems: function() {
-                return axios
-                .get('products.json')
-                .then(response => 
-                {
-                    this.filteredShop = response.data.categories;
-                })
+            methods: 
+            {
+                shopItems: function() {
+                    return axios
+                    .get('products.json')
+                    .then(response => 
+                    {
+                        this.filteredShop = response.data.categories;
+                    })
             }
 
         },
-        template: '<div class=shopItems style="width: 250px;">'
-        + '<div v-for="(val,key) in filteredShop" style="width: 250px;"> '
-        + '<div v-for="val2 in val" style="width: 250px;"> '
-        + '<div v-for="val3 in val2" style="width: 250px;" v-if="val3.firstpage === true"> ' //välj vilka som ska dyka upp
+        template: '<div class=shopItems>'
+        + '<div v-for="(val,key) in filteredShop"> '
+        + '<div v-for="val2 in val"> '
+        + '<div v-for="val3 in val2" v-if="val3.firstpage === true"> ' //välj vilka som ska dyka upp
 
-        
-        + '<img :src=val3.Img style="height: 300px;"></img>'
-        + '<p>{{val3.Brand}}</p>'
-        + '<p>{{val3.Price}} sek</p>'
+        //detta fungerar
+        // + '<img :src=val3.Img style="height: 300px;"></img>'
+        // + '<p>{{val3.Brand}}</p>'
+        // + '<p>{{val3.Price}} sek</p>'
+        // + '<p>{{val3.Description}}</p>'
+        // + '<button>Köp</button>'
+
+        //testing
+        + '<div class="col-lg-4 col-md-4 all des">'
+        + '<div class="product-item">'
+        + '<a href="#" class="text-in-productbox"><img :src=val3.Img alt=""></a>'
+        + '<div class="down-content">'
+        + '<a href="#"><h4>{{val3.Brand}}</h4></a>'
+        + '<h6>{{val3.Price}} sek</h6>'
         + '<p>{{val3.Description}}</p>'
         + '<button>Köp</button>'
+        + '</div></div></div>'
+
+
+
+
+    //     <div class="row grid">
+    //     <div class="col-lg-4 col-md-4 all des">
+    //     <div class="product-item">
+    //       <!-- <filtreraJacka></filtreraJacka> -->
+    //       <a href="#" class="text-in-productbox"><img src="images/kavaj.jpg" alt=""></a>
+    //       <div class="down-content">
+    //         <a href="#"><h4>Tittle goes here</h4></a>
+    //         <h6>$18.25</h6>
+    //         <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+            
+    //       </div>
+    //     </div>
+    //   </div>
+    //   </div>
 
 
         + '</div></div></div></div>'
     })
 
 //     var app = new Vue({ el: '#app' })    
+
+
+// PRODUKT SIDAN
+// let retuneradeDatan
+
+// Vue.component('filtreraJacka',
+// {
+//     data:function () {
+//         return {
+//             filteredShop: this.shopItems()
+//             }
+                
+//         },
+//     methods: 
+//         {
+//             shopItems: function() 
+//             {
+//                     return axios
+//                     .get('products.json')
+//                     .then(response => 
+//                     {
+//                         retuneradeDatan = response.data.categories;
+//                     })
+//             }
+
+//         },
+//         template: '<div class=shopItems style="width: 250px;">'
+//         + '<div v-for="(val,key) in retuneradeDatan" style="width: 250px;"> '
+//         + '<div v-for="val2 in val" style="width: 250px;"> '
+//         + '<div v-for="val3 in val2" style="width: 250px;" v-if="val3.Brand === "Jacka""> ' //välj vilka som ska dyka upp
+
+        
+//         + '<a href=#><img :src=val3.Img style="height: 300px;"></img></a>'
+//         + '<p>{{val3.Brand}}</p>'
+//         + '<p>{{val3.Price}} sek</p>'
+//         + '<p>{{val3.Description}}</p>'
+//         + '<button>Köp</button>'
+
+
+//         + '</div></div></div></div>'
+// })
